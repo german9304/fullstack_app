6111c1b0da2e95f22ff24803181e2c6cbc0ad98c
German Razo Rodriguez <ger9366@gmail.com>
2020-01-03 09:45:31 -0800

    faked user file

---
+++ frontend/__testing__/fakeUser.js
@@ -0,0 +1,12 @@
+const fakeUser = () => ({
+  author_id: '1234',
+  last_name: 'martinez',
+  age: 32,
+  name: 'german',
+  email: 'german@mail.com',
+  password: 'password234',
+  created: '2019-23-10',
+  books: []
+});
+
+export { fakeUser };
